6a925efe9bd15724c82191fd1e8614a4334a949f
MAR BEN <marouanebenbajja@gmail.com>
2022-06-23 18:42:43 +0100

    Executing pwd

--- execution/builtins/ft_pwd.c
+++ execution/builtins/ft_pwd.c
@@ -1,2 +1,22 @@
 #include "../headers/execution.h"
 
+int	ft_pwd(t_shell *shell)
+{
+	t_env	*env;
+	char	*path;
+	char	cwd[PATH_MAX];
+
+	env = shell->env;
+	while (env)
+	{
+		if (!ft_strcmp("PWD", env->var))
+			break ;
+		env = env->next;
+	}
+	if (env)
+		path = env->value;
+	else
+		path = getcwd(cwd, sizeof(cwd));
+	printf("%s\n", path);
+	return (0);
+}
--- execution/utils/proccess_buffer.c
+++ execution/utils/proccess_buffer.c
@@ -45,12 +45,12 @@ int	proccess_cmd(t_shell *shell, char *cmd)
 		ft_echo(shell);
 	else if (!ft_strcmp(cmd, "env"))
 		ft_env(shell);
+	else if (!ft_strcmp(cmd, "pwd"))
+		ft_pwd(shell);
 	// else if (!ft_strcmp(cmd, "exit"))
 	// 	ft_exit(shell);
 	// else if (!ft_strcmp(cmd, "export"))
 	// 	ft_export(shell);
-	// else if (!ft_strcmp(cmd, "pwd"))
-	// 	ft_pwd(shell);
 	// else if (!ft_strcmp(cmd, "unset"))
 	// 	ft_unset(shell);
 	else
