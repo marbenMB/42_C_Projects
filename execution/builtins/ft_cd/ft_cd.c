e6f8e1e2925311a276b017b785e2a028e84133de
marbenMB <mbenbajj@student.1337.ma>
2022-06-30 15:32:20 +0100

    Starting CD -> ...

--- execution/builtins/ft_cd/ft_cd.c
+++ execution/builtins/ft_cd/ft_cd.c
@@ -6,19 +6,36 @@
 /*   By: mbenbajj <mbenbajj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 13:44:34 by mbenbajj          #+#    #+#             */
-/*   Updated: 2022/06/28 13:48:10 by mbenbajj         ###   ########.fr       */
+/*   Updated: 2022/06/30 15:30:28 by mbenbajj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/execution.h"
 
-int	ft_cd(t_shell *shell)
+int	check_path(char *path)
 {
-	char	*path;
+	if (path && path[0])
+	{
+		if (path[0] == '-' && ft_strlen(path) > 1)
+			return (0);
+		
+	}
+	return (0);
+}
+
+int ft_cd(t_shell *shell)
+{
+	char *path;
 
 	path = shell->cmd->cmd_flags[1];
+	if (check_path(path))
+	{
+		ft_status(&shell->env, "1");	
+		return (1);
+	}
 	if (!path)
 	{
+		//	if !HOME -> bash : cd : HOME not set
 		//	cd -> HOME
 		/* 	set -> NEW_ENV
 		{
@@ -26,8 +43,9 @@ int	ft_cd(t_shell *shell)
 				set -> NEW_OLDPWD
 		} */
 	}
-	else if (!ft_strcmp(path, "-") || !ft_strcmp(path, "-"))
+	else if (!ft_strcmp(path, "-"))
 	{
+		//	if !OLDPWD -> bash : cd : OLDPWD not set
 		//	cd -> OLDPWD
 		/* 	set -> NEW_ENV
 		{
